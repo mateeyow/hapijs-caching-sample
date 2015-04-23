9f8be8bd3adbfe2855d29692614ceb66bf479552
Matthew Torres <matthewt@meditab.com>
2015-04-23 13:20:39 +0800

    add waterline or dogwater testing

--- index.js
+++ index.js
@@ -6,98 +6,174 @@ global.__basePath = process.cwd();
 var Hapi = require('hapi'),
   cRedis = require('catbox-redis'),
   // routes = require(__basePath+'/routes'),
-  Cache  = require(__basePath+'/cache');
+  Cache  = require(__basePath+'/cache'),
+  colors = require('colors');
 
 // Temporary - should be cleaned
 var Boom    = require('boom'),
   mongoose  = require('mongoose'),
   addr      = process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost',
   port      = process.env.MONGO_PORT_27017_TCP_PORT || 27017;
+var redisHost = process.env.REDIS_PORT_6379_TCP_ADDR || 'localhost';
 var Schema  = mongoose.Schema;
 
-mongoose.connect('mongodb://'+addr+':'+port+'/hapicaching');
-var testingSchema = new Schema({name: String});
-var Testing = mongoose.model('testing', testingSchema);
-
+var ErrorReply = function (err) {
+  this.error = err
+  return Boom.create(this.error.status, this.error.reason);
+}
 // Variable declarations and initialization
-var redisHost = process.env.REDIS_PORT_6379_TCP_ADDR || 'localhost';
-server = new Hapi.Server({
-  cache: {
-    name      : 'redis',
-    engine    : require('catbox-mongodb'),
-    host      : addr,
-    partition : 'mongoCaching'
-  }
-});
+server = new Hapi.Server();
 
 server.connection({port:3005});
-var Find = function (params, next) {
-  Testing.find().exec(function (err, result) {
-    if (result.length > 0) {
-      data = {
-        count : result.length,
-        data  : result
-      };
-      return next(null, data);
-    }
-  });
-};
 
-// server.method('find', Find, {cache: {cache: 'redis', expiresIn: 5*1000}});
-server.method([
-  {
-    name: 'find',
-    method: Find,
-    options: {
-      cache: {
-        cache     : 'redis',
-        expiresIn : 20 * 1000
-      },
-      generateKey: function (para) {
-        return para.model;
+server.register({
+  register: require('dogwater'),
+  options: {
+    connections: {
+      'mongoConn': {
+        adapter: 'mongoConn',
+        host: addr,
+        database: 'dogwaterOrm'
       }
-    }
+    },
+    adapters: {
+      mongoConn: 'sails-mongo'
+    },
+    models: [
+      {
+        identity: 'user',
+        schema: true,
+        migrate: 'safe',
+        connection: 'mongoConn',
+        attributes: {
+          firstName: 'string',
+          lastName: 'string',
+          age: {
+            type: 'integer',
+            required: true
+          },
+          status: 'boolean',
+          company: {
+            model: 'company'
+          }
+        }
+      },
+      {
+        identity: 'company',
+        schema: true,
+        migrate: 'safe',
+        connection: 'mongoConn',
+        attributes: {
+          users: {
+            collection: 'user',
+            via: 'company'
+          },
+          name: 'string'
+        }
+      }
+    ]
   }
-]);
+}, function (err) {
+  if (err) console.log(colors.red.bold('Failed to load dogwater: ', err))
+});
 
 server.route([
   {
-    method  : 'GET',
-    path    : '/',
-    handler : function (req, res) {
-      res('Yow');
-    }
-  },
-  {
-    method  : '*',
-    path    : '/something',
-    handler : function (req, res) {
-      var param = {model: 'testing'};
-      var random = 'Testing' + Math.floor(Math.random() * 100);
-      Testing.create({name: random}, function (err, response) {
-        return res(err, response);
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      var User = request.model.user
+      User.find().exec(function (err, user) {
+        reply(err, user);
       });
-
-      var param = {model: 'testing'};
-      return server.methods.find.cache.drop(param, function (err, result, cache, report) {});
     }
   },
   {
-    method  : '*',
-    path    : '/{model}/list',
-    handler : function (req, res) {
-      server.methods.find(req.params, function (err, result, cache, report) {
-        if (cache) console.log('cache ok');
-        console.log(report);
-        return res(result);
+    method: 'GET',
+    path: '/create',
+    handler: function (request, reply) {
+      var User = request.model.user
+      var data = {
+        firstName: 'Joe',
+        lastName: 'Manyakols',
+        age: 26,
+        sex: 'male',
+        company: '1'
+      }
+      User.create(data).exec(function (err, user) {
+        if (err) {
+          reply(new ErrorReply(err))
+        } else {
+          reply(user);
+        }
+      })
+    }
+  },
+  {
+    method: 'GET',
+    path: '/company/create',
+    handler: function (request, reply) {
+      var Company = request.model.company
+      var data = {
+        name: 'Meditab'
+      }
+      Company.create(data)
+      .exec(function (err, company) {
+        if (err) reply(new ErrorReply(err));
+        if (company) reply(company);
+      });
+    }
+  },
+  {
+    method: 'GET',
+    path: '/company/list',
+    handler: function (request, reply) {
+      var Company = request.model.company
+      var yow = Company.find()
+      hey = true
+      if (hey) {
+        sass = yow.populate('users')
+      }
+      sass.exec(function (err, company) {
+        if (err) reply(new ErrorReply(err));
+        if (company) reply(company);
+      });
+    }
+  },
+  {
+    method: 'GET',
+    path: '/delete',
+    handler: function (request, reply) {
+      var User = request.model.user
+      User.destroy({age: 26}).exec(function (err, user) {
+        if (err) {
+          reply (err);
+        } else {
+          reply(user);
+        }
+      })
+    }
+  },
+  {
+    method: 'GET',
+    path: '/one',
+    handler: function (request, reply) {
+      var User = request.model.user
+      User.findOne({firstName: 'Joe'})
+      .populate('company')
+      .exec(function (err, user) {
+        console.log(err, user);
+        if (err) {
+          reply(new ErrorReply(err));
+        } else {
+          reply(user);
+        }
       });
     }
   }
-]);
-
-// server.route(routes);
+])
 
 server.start(function (err) {
   if (err) console.log(err);
-  console.log('Server running at:', server.info.uri);
+  console.log(colors.green.bold('Server running at:', server.info.uri));
 });
---
+++ indexCopy.js
@@ -0,0 +1,103 @@
+// Global variables
+// WARNING: Use with discretion
+global.__basePath = process.cwd();
+
+// All require variables
+var Hapi = require('hapi'),
+  cRedis = require('catbox-redis'),
+  // routes = require(__basePath+'/routes'),
+  Cache  = require(__basePath+'/cache');
+
+// Temporary - should be cleaned
+var Boom    = require('boom'),
+  mongoose  = require('mongoose'),
+  addr      = process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost',
+  port      = process.env.MONGO_PORT_27017_TCP_PORT || 27017;
+var Schema  = mongoose.Schema;
+
+mongoose.connect('mongodb://'+addr+':'+port+'/hapicaching');
+var testingSchema = new Schema({name: String});
+var Testing = mongoose.model('testing', testingSchema);
+
+// Variable declarations and initialization
+var redisHost = process.env.REDIS_PORT_6379_TCP_ADDR || 'localhost';
+server = new Hapi.Server({
+  cache: {
+    name      : 'redis',
+    engine    : require('catbox-redis'),
+    host      : redisHost,
+    partition : 'hapiCaching'
+  }
+});
+
+server.connection({port:3005});
+var Find = function (params, next) {
+  Testing.find().exec(function (err, result) {
+    if (result.length > 0) {
+      data = {
+        count : result.length,
+        data  : result
+      };
+      return next(null, data);
+    }
+  });
+};
+
+server.method([
+  {
+    name: 'find',
+    method: Find,
+    options: {
+      cache: {
+        cache     : 'redis',
+        expiresIn : 20 * 1000,
+      },
+      generateKey: function (para) {
+        // console.log(para.model);
+        return para.model;
+      }
+    }
+  }
+]);
+
+server.route([
+  {
+    method  : 'GET',
+    path    : '/',
+    handler : function (req, res) {
+      res('Yow');
+    }
+  },
+  {
+    method  : '*',
+    path    : '/something',
+    handler : function (req, res) {
+      var param = {model: 'testing'};
+      var random = 'Testing' + Math.floor(Math.random() * 100);
+      Testing.create({name: random}, function (err, response) {
+        return res(err, response);
+      });
+
+      var param = {model: 'testing'};
+      return server.methods.find.cache.drop(param, function (err, result, cache, report) {});
+    }
+  },
+  {
+    method  : '*',
+    path    : '/{model}/list',
+    handler : function (req, res) {
+      server.methods.find(req.params, function (err, result, cache, report) {
+        if (cache) console.log('cache ok');
+        console.log(report);
+        return res(result);
+      });
+    }
+  }
+]);
+
+// server.route(routes);
+
+server.start(function (err) {
+  if (err) console.log(err);
+  console.log('Server running at:', server.info.uri);
+});
